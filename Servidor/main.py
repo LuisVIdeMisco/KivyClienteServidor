d8cbb3fa622b84bf442489896e1b36d7d4a3d206
LuisVIdeMisco <95245754+LuisVIdeMisco@users.noreply.github.com>
2022-06-26 20:42:15 +0200

    Clase servidor
    La interfaz y la funcionalidad del servidor se han dividido

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from kivy.config import Config
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '150')
@@ -9,9 +10,17 @@ import sys
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
 from kivy.properties import StringProperty
 
-class Servidor(BoxLayout):
+from kivy.metrics import dp
+
+import Servidor
+
+class ServidorInterf(BoxLayout):
     ruta_base = StringProperty("./FicherosServidor")
     datos_raiz = '.metadata'
     hilos = []
@@ -19,168 +28,40 @@ class Servidor(BoxLayout):
     port = StringProperty("")
     error = StringProperty("")
 
-    def carga(self, con, carpeta):
-        if not os.path.isdir(carpeta):
-            os.makedirs(carpeta, 0o777)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        self.padding = 0.2
 
-        global datos_raiz
-        md = carpeta + "/" + self.datos_raiz
-        guardados = []
-        if not os.path.exists(md):
-            fp = open(md, 'x')
-            fp.close()
-        else:
-            fp = open(md, 'r')
-            for line in fp.readlines():
-                guardados.append(line[:-1])
+        blIP = BoxLayout(height=dp(40), width=dp(500))
+        etIP = Label(text="Dirección IP: ")
+        tiIP = TextInput(text=self.dir_ip, multiline=False, hint_text="127.0.0.1")
+        blIP.add_widget(etIP)
+        blIP.add_widget(tiIP)
 
-        try:
-            n = con.recv(4)
-            cantidad = int.from_bytes(n, byteorder='big', signed=False)
+        blPort = BoxLayout(height=dp(40), width=dp(500))
+        etPort = Label(text="Puerto: ")
+        tiPort = TextInput(text=self.port, multiline=False, hint_text="12400")
+        blPort.add_widget(etPort)
+        blPort.add_widget(tiPort)
 
-            meta = open(md, 'a')
+        blInt = BoxLayout(height=dp(40))
+        etErr = Label(text=self.error)
+        bInt = Button(text="Arrancar", size_hint=(0.25, 0.7))
+        bInt.bind(on_press=self.iniciar)
+        blInt.add_widget(etErr)
+        blInt.add_widget(bInt)
 
-            for i in range(cantidad):
-                datos = con.recv(4)
-                longitud = int.from_bytes(datos, byteorder='big', signed=False)
-                datos = con.recv(4)
-                longitudFich = int.from_bytes(datos, byteorder='big', signed=False)
-
-                nombre_fich = con.recv(longitud).decode()
-
-                try:
-                    if nombre_fich in guardados:
-                        name, ext = nombre_fich.split('.')
-                        num = 1
-                        while nombre_fich in guardados:
-                            nombre_fich = name + '(' + str(num) + ').' + ext
-                            num += 1
-
-                    meta.write(nombre_fich + '\n')
-                    f = open(carpeta + '/' + nombre_fich, 'wb')
-
-                    amount_received = 0
-                    amount_expected = longitudFich
-
-                    while amount_received < amount_expected:
-                        data = con.recv(amount_expected - amount_received)
-                        amount_received += len(data)
-                        f.write(data)
-                except:
-                    break
-
-                finally:
-                    f.close()
-                    con.send(b'1')
-
-        finally:
-            meta.close()
-            con.close()
-
-
-    def info(con, carpeta):
-        if not os.path.isdir(carpeta):
-            os.makedirs(carpeta, 0o777)
-
-        global datos_raiz
-        md = carpeta + "/" + datos_raiz
-        if not os.path.exists(md):
-            fp = open(md, 'x')
-            fp.close()
-
-        tam = os.path.getsize(md)
-        file_size = tam.to_bytes(4, 'big', signed=False)
-        con.send(file_size)
-
-        amount_sent = 0
-        meta = open(md, 'rb')
-        while amount_sent < tam:
-            d = meta.read(1460)
-            numbytes = con.send(d)
-            amount_sent += numbytes
-
-        meta.close()
-        while con.recv(1) == 0:
-            pass
-
-        con.close()
-
-
-    def remove(con, carpeta):
-        global datos_raiz
-        md = carpeta + "/" + datos_raiz
-        meta = open(md, 'r')
-        guardados = []
-        for line in meta.readlines():
-            guardados.append(line[:-1])
-
-        meta.close()
-
-        n = con.recv(4)
-        cantidad = int.from_bytes(n, byteorder='big', signed=False)
-
-        for i in range(cantidad):
-            datos = con.recv(4)
-            longitud = int.from_bytes(datos, byteorder='big', signed=False)
-
-            nombre_fich = con.recv(longitud).decode()
-            guardados.remove(nombre_fich)
-
-            os.remove(carpeta + '/' + nombre_fich)
-
-            con.send(b'1')
-
-        meta = open(md, 'w')
-        meta.truncate(0)
-        for line in guardados:
-            meta.write(line + '\n')
-
-        con.close()
-
-
-    def download(con, carpeta):
-        n = con.recv(4)
-        cantidad = int.from_bytes(n, byteorder='big', signed=False)
-
-        cantDwn = 0
-        ficheros = []
-        for i in range(cantidad):
-            datos = con.recv(4)
-            longitud = int.from_bytes(datos, byteorder='big', signed=False)
-
-            nombre_fich = con.recv(longitud).decode()
-            nombre = carpeta + '/' + nombre_fich
-
-            long = os.path.getsize(nombre)
-            cantDwn += long
-            ficheros.append(nombre)
-
-        cantDwn = cantDwn.to_bytes(4, 'big', signed=False)
-        con.send(cantDwn)
-
-        for nombre in ficheros:
-            long = os.path.getsize(nombre)
-            tam = long.to_bytes(4, 'big', signed=False)
-            con.send(tam)
-
-            fd = open(nombre, 'rb')
-
-            amount_sent = 0
-            while amount_sent < long:
-                d = fd.read()
-                numbytes = con.send(d)
-                amount_sent += numbytes
-
-            while con.recv(1) == 0:
-                pass
-
-        con.close()
-
-    def arrancar(self):
-        global dir_ip
-
+        self.add_widget(blIP)
+        self.add_widget(blPort)
+        self.add_widget(blInt)
+    
+    def iniciar(self, instance):
+        self.error = "Iniciando"
+        print(self.error)
 
 class ServidorApp(App):
-    pass
+    def build(self):
+        return ServidorInterf()
 
 ServidorApp().run()
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -4,220 +4,236 @@ import os
 import threading
 import sys
 
-ruta_base = './FicherosServidor'
-datos_raiz = '.metadata'
-hilos = []
+encendido = False
 
-if not os.path.isdir(ruta_base):
-    os.makedirs(ruta_base, 0o777)
+class Servidor(threading.Thread):
+    dir_ip = "localhost"
+    port = 12400
+    ruta_base = './FicherosServidor'
+    datos_raiz = '.metadata'
+    hilos = []
+
+    context = ""
+    sock = ""
+    ssock = ""
+    encendido = True
+
+    def __init__(self, dir_ip="localhost", port=12400):
+        super().__init__()
+        self.dir_ip = dir_ip
+        self.port = port
+
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        self.context.verify_mode = ssl.CERT_REQUIRED
+        self.context.load_cert_chain('serverCert.pem', 'claveServer.pem')
+        self.context.load_verify_locations('certCA.pem')
+
+        # Creamos el socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ssock = self.context.wrap_socket(self.sock)
+
+        server_address = (self.dir_ip, self.port)
+        self.ssock.bind(server_address)
+
+        self.ssock.listen(5)
 
 
-def carga(con, carpeta):
-    if not os.path.isdir(carpeta):
-        os.makedirs(carpeta, 0o777)
+    def setDir(self, ruta_base):
+        self.ruta_base = ruta_base
+        if not os.path.isdir(ruta_base):
+            os.makedirs(ruta_base, 0o777)
 
-    global datos_raiz
-    md = carpeta + "/" + datos_raiz
-    guardados = []
-    if not os.path.exists(md):
-        fp = open(md, 'x')
-        fp.close()
-    else:
-        fp = open(md, 'r')
-        for line in fp.readlines():
+
+    def carga(self, con, carpeta):
+        if not os.path.isdir(carpeta):
+            os.makedirs(carpeta, 0o777)
+
+        datos_raiz = self.datos_raiz
+        md = carpeta + "/" + datos_raiz
+        guardados = []
+        if not os.path.exists(md):
+            fp = open(md, 'x')
+            fp.close()
+        else:
+            fp = open(md, 'r')
+            for line in fp.readlines():
+                guardados.append(line[:-1])
+
+        try:
+            n = con.recv(4)
+            cantidad = int.from_bytes(n, byteorder='big', signed=False)
+
+            meta = open(md, 'a')
+
+            for i in range(cantidad):
+                datos = con.recv(4)
+                longitud = int.from_bytes(datos, byteorder='big', signed=False)
+                datos = con.recv(4)
+                longitudFich = int.from_bytes(datos, byteorder='big', signed=False)
+
+                nombre_fich = con.recv(longitud).decode()
+
+                try:
+                    if nombre_fich in guardados:
+                        name, ext = nombre_fich.split('.')
+                        num = 1
+                        while nombre_fich in guardados:
+                            nombre_fich = name + '(' + str(num) + ').' + ext
+                            num += 1
+
+                    meta.write(nombre_fich + '\n')
+                    f = open(carpeta + '/' + nombre_fich, 'wb')
+
+                    amount_received = 0
+                    amount_expected = longitudFich
+
+                    while amount_received < amount_expected:
+                        data = con.recv(amount_expected - amount_received)
+                        amount_received += len(data)
+                        f.write(data)
+                except:
+                    break
+
+                finally:
+                    f.close()
+                    con.send(b'1')
+
+        finally:
+            meta.close()
+            con.close()
+
+
+    def info(self, con, carpeta):
+        if not os.path.isdir(carpeta):
+            os.makedirs(carpeta, 0o777)
+
+        datos_raiz = self.datos_raiz
+        md = carpeta + "/" + datos_raiz
+        if not os.path.exists(md):
+            fp = open(md, 'x')
+            fp.close()
+
+        tam = os.path.getsize(md)
+        file_size = tam.to_bytes(4, 'big', signed=False)
+        con.send(file_size)
+
+        amount_sent = 0
+        meta = open(md, 'rb')
+        while amount_sent < tam:
+            d = meta.read(1460)
+            numbytes = con.send(d)
+            amount_sent += numbytes
+
+        meta.close()
+        while con.recv(1) == 0:
+            pass
+
+        con.close()
+
+
+    def remove(self, con, carpeta):
+        datos_raiz = self.datos_raiz
+        md = carpeta + "/" + datos_raiz
+        meta = open(md, 'r')
+        guardados = []
+        for line in meta.readlines():
             guardados.append(line[:-1])
 
-    try:
+        meta.close()
+
         n = con.recv(4)
         cantidad = int.from_bytes(n, byteorder='big', signed=False)
-
-        meta = open(md, 'a')
 
         for i in range(cantidad):
             datos = con.recv(4)
             longitud = int.from_bytes(datos, byteorder='big', signed=False)
-            datos = con.recv(4)
-            longitudFich = int.from_bytes(datos, byteorder='big', signed=False)
 
             nombre_fich = con.recv(longitud).decode()
+            guardados.remove(nombre_fich)
 
-            try:
-                if nombre_fich in guardados:
-                    name, ext = nombre_fich.split('.')
-                    num = 1
-                    while nombre_fich in guardados:
-                        nombre_fich = name + '(' + str(num) + ').' + ext
-                        num += 1
+            os.remove(carpeta + '/' + nombre_fich)
 
-                meta.write(nombre_fich + '\n')
-                f = open(carpeta + '/' + nombre_fich, 'wb')
+            con.send(b'1')
 
-                amount_received = 0
-                amount_expected = longitudFich
+        meta = open(md, 'w')
+        meta.truncate(0)
+        for line in guardados:
+            meta.write(line + '\n')
 
-                while amount_received < amount_expected:
-                    data = con.recv(amount_expected - amount_received)
-                    amount_received += len(data)
-                    f.write(data)
-            except:
-                break
-
-            finally:
-                f.close()
-                con.send(b'1')
-
-    finally:
-        meta.close()
         con.close()
 
 
-def info(con, carpeta):
-    if not os.path.isdir(carpeta):
-        os.makedirs(carpeta, 0o777)
+    def download(self, con, carpeta):
+        n = con.recv(4)
+        cantidad = int.from_bytes(n, byteorder='big', signed=False)
 
-    global datos_raiz
-    md = carpeta + "/" + datos_raiz
-    if not os.path.exists(md):
-        fp = open(md, 'x')
-        fp.close()
+        cantDwn = 0
+        ficheros = []
+        for i in range(cantidad):
+            datos = con.recv(4)
+            longitud = int.from_bytes(datos, byteorder='big', signed=False)
 
-    tam = os.path.getsize(md)
-    file_size = tam.to_bytes(4, 'big', signed=False)
-    con.send(file_size)
+            nombre_fich = con.recv(longitud).decode()
+            nombre = carpeta + '/' + nombre_fich
 
-    amount_sent = 0
-    meta = open(md, 'rb')
-    while amount_sent < tam:
-        d = meta.read(1460)
-        numbytes = con.send(d)
-        amount_sent += numbytes
+            long = os.path.getsize(nombre)
+            cantDwn += long
+            ficheros.append(nombre)
 
-    meta.close()
-    while con.recv(1) == 0:
-        pass
+        cantDwn = cantDwn.to_bytes(4, 'big', signed=False)
+        con.send(cantDwn)
 
-    con.close()
+        for nombre in ficheros:
+            long = os.path.getsize(nombre)
+            tam = long.to_bytes(4, 'big', signed=False)
+            con.send(tam)
 
+            fd = open(nombre, 'rb')
 
-def remove(con, carpeta):
-    global datos_raiz
-    md = carpeta + "/" + datos_raiz
-    meta = open(md, 'r')
-    guardados = []
-    for line in meta.readlines():
-        guardados.append(line[:-1])
+            amount_sent = 0
+            while amount_sent < long:
+                d = fd.read()
+                numbytes = con.send(d)
+                amount_sent += numbytes
 
-    meta.close()
+            while con.recv(1) == 0:
+                pass
 
-    n = con.recv(4)
-    cantidad = int.from_bytes(n, byteorder='big', signed=False)
+        con.close()
+    
+    def run(self):
+        ruta = self.ruta_base
+        while self.encendido:
+            try:
+                conexion, dir_client = self.ssock.accept()
+            except:
+                print(sys.exc_info())
+                continue
 
-    for i in range(cantidad):
-        datos = con.recv(4)
-        longitud = int.from_bytes(datos, byteorder='big', signed=False)
+            func = conexion.recv(1).decode()
 
-        nombre_fich = con.recv(longitud).decode()
-        guardados.remove(nombre_fich)
+            if func == 'U':     # Upload
+                # carga(conexion, ruta_base + '/' + dir_client[0])
+                th = threading.Thread(target=self.carga, args=(conexion, str(ruta) + '/' + str(dir_client[0])), daemon=True)
+            elif func == 'I':
+                th = threading.Thread(target=self.info, args=(conexion, str(ruta) + '/' + str(dir_client[0])), daemon=True)
+            elif func == 'R':
+                th = threading.Thread(target=self.remove, args=(conexion, str(ruta) + '/' + str(dir_client[0])), daemon=True)
+            elif func == 'D':
+                th = threading.Thread(target=self.download, args=(conexion, str(ruta) + '/' + str(dir_client[0])), daemon=True)
+            else:
+                th = None
 
-        os.remove(carpeta + '/' + nombre_fich)
+            if not th == None:
+                self.hilos.append(th)
+                th.start()
 
-        con.send(b'1')
+            for th in self.hilos:
+                if not th.is_alive():
+                    th.join()
+                    self.hilos.remove(th)
 
-    meta = open(md, 'w')
-    meta.truncate(0)
-    for line in guardados:
-        meta.write(line + '\n')
-
-    con.close()
-
-
-def download(con, carpeta):
-    n = con.recv(4)
-    cantidad = int.from_bytes(n, byteorder='big', signed=False)
-
-    cantDwn = 0
-    ficheros = []
-    for i in range(cantidad):
-        datos = con.recv(4)
-        longitud = int.from_bytes(datos, byteorder='big', signed=False)
-
-        nombre_fich = con.recv(longitud).decode()
-        nombre = carpeta + '/' + nombre_fich
-
-        long = os.path.getsize(nombre)
-        cantDwn += long
-        ficheros.append(nombre)
-
-    cantDwn = cantDwn.to_bytes(4, 'big', signed=False)
-    con.send(cantDwn)
-
-    for nombre in ficheros:
-        long = os.path.getsize(nombre)
-        tam = long.to_bytes(4, 'big', signed=False)
-        con.send(tam)
-
-        fd = open(nombre, 'rb')
-
-        amount_sent = 0
-        while amount_sent < long:
-            d = fd.read()
-            numbytes = con.send(d)
-            amount_sent += numbytes
-
-        while con.recv(1) == 0:
-            pass
-
-    con.close()
+    def terminar(self):
+        self.encendido = False
 
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.verify_mode = ssl.CERT_REQUIRED
-context.load_cert_chain('serverCert.pem', 'claveServer.pem')
-context.load_verify_locations('certCA.pem')
-
-# Creamos el socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssock = context.wrap_socket(sock)
-
-# Establecemos la direccion y el puerto del servidor
-dir_ip = 'localhost'
-serv_port = 12400
-server_address = (dir_ip, serv_port)
-ssock.bind(server_address)
-
-print('Servidor disponible en {}:{}'.format(*server_address))
-
-ssock.listen(5)
-
-print("Esperando conexiones")
-
-ruta = ruta_base
-while True:
-    try:
-        conexion, dir_client = ssock.accept()
-    except:
-        print(sys.exc_info())
-        continue
-
-    func = conexion.recv(1).decode()
-
-    if func == 'U':     # Upload
-        # carga(conexion, ruta_base + '/' + dir_client[0])
-        th = threading.Thread(target=carga, args=(conexion, str(ruta_base) + '/' + str(dir_client[0])), daemon=True)
-    elif func == 'I':
-        th = threading.Thread(target=info, args=(conexion, str(ruta_base) + '/' + str(dir_client[0])), daemon=True)
-    elif func == 'R':
-        th = threading.Thread(target=remove, args=(conexion, str(ruta_base) + '/' + str(dir_client[0])), daemon=True)
-    elif func == 'D':
-        th = threading.Thread(target=download, args=(conexion, str(ruta_base) + '/' + str(dir_client[0])), daemon=True)
-    else:
-        th = None
-
-    if not th == None:
-        hilos.append(th)
-        th.start()
-
-    for th in hilos:
-        if not th.is_alive():
-            th.join()
-            hilos.remove(th)
