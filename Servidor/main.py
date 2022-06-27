905eba9fdeb40897ce0c6a2c225cbff7ed55491e
LuisVIdeMisco <95245754+LuisVIdeMisco@users.noreply.github.com>
2022-06-27 22:10:59 +0200

    Actualización valores
    Se tienen que almacenar los widgets, eso es malo, pero al menos podemos obtener los valores que se les introducen y modificarlos, eso es bueno

--- Servidor/main.py
+++ Servidor/main.py
@@ -10,6 +10,7 @@ import sys
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -24,9 +25,13 @@ class ServidorInterf(BoxLayout):
     ruta_base = StringProperty("./FicherosServidor")
     datos_raiz = '.metadata'
     hilos = []
-    dir_ip = StringProperty("")
-    port = StringProperty("")
-    error = StringProperty("")
+    #dir_ip = StringProperty("")
+    #port = StringProperty("")
+    #error = StringProperty("Error")
+    
+    tiIP = ""
+    tiPort = ""
+    etErr = ""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -35,21 +40,20 @@ class ServidorInterf(BoxLayout):
 
         blIP = BoxLayout(height=dp(40), width=dp(500))
         etIP = Label(text="Dirección IP: ")
-        tiIP = TextInput(text=self.dir_ip, multiline=False, hint_text="127.0.0.1")
+        self.tiIP = TextInput(multiline=False, hint_text="127.0.0.1")
         blIP.add_widget(etIP)
-        blIP.add_widget(tiIP)
+        blIP.add_widget(self.tiIP)
 
         blPort = BoxLayout(height=dp(40), width=dp(500))
         etPort = Label(text="Puerto: ")
-        tiPort = TextInput(text=self.port, multiline=False, hint_text="12400")
+        self.tiPort = TextInput(multiline=False, hint_text="12400")
         blPort.add_widget(etPort)
-        blPort.add_widget(tiPort)
+        blPort.add_widget(self.tiPort)
 
         blInt = BoxLayout(height=dp(40))
-        etErr = Label(text=self.error)
-        bInt = Button(text="Arrancar", size_hint=(0.25, 0.7))
-        bInt.bind(on_press=self.iniciar)
-        blInt.add_widget(etErr)
+        self.etErr = Label()
+        bInt = Button(text="Arrancar", size_hint=(0.25, 0.7), on_press=self.iniciar)
+        blInt.add_widget(self.etErr)
         blInt.add_widget(bInt)
 
         self.add_widget(blIP)
@@ -57,8 +61,11 @@ class ServidorInterf(BoxLayout):
         self.add_widget(blInt)
     
     def iniciar(self, instance):
-        self.error = "Iniciando"
-        print(self.error)
+        self.etErr.text = "Iniciando"
+        print(self.tiIP.text)
+
+class Manejador(ScreenManager):
+    pass
 
 class ServidorApp(App):
     def build(self):
